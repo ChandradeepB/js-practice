7cc10066ded809b6661d7c4bab8e335b3ba06960
ChandradeepB <107803508+ChandradeepB@users.noreply.github.com>
2025-05-09 13:05:23 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3);
+
+console.log("Chandradeep");
+
+//tc39.es website used for js
+
+let name = "Chandradeep"
+let age = 18
+let isLoggedIn = false
+let state;
+
+/* primitive datatypes */
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique(react)
+
+/* object */
+
+console.log(typeof "age"); //string
+console.log(typeof age); //number
+console.log(typeof undefined); //undefined
